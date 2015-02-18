7dcbb3d990a5b35969a981a6fa6e0972098c95cc
ripan <er.ripankumar@gmail.com>
2015-02-18 16:58:43 +0000

    Show 70000 frames

--- app/js/webgl/map.js
+++ app/js/webgl/map.js
@@ -8,8 +8,8 @@
          center: center,
          mapTypeId: google.maps.MapTypeId.ROADMAP,
          disableDefaultUI: true,
-         draggable: false,
-         scrollwheel: false,
+         draggable: true,
+         scrollwheel: true,
          panControl: false
      };
      var mapCanvas = document.getElementById('header-map');
@@ -24,14 +24,53 @@
      // myLayer.tilebase = 'http://130.211.58.85:8080/postcodes/';
      // myLayer.zoomlock = 12;
 
-     myLayer.loadGeoJson('/data/postcodes.geojson');
+     // myLayer.loadGeoJson('/data/postcodes.geojson');
 
      // google.maps.event.addListenerOnce(map, 'idle', function(){
      //   generateData(100000);
      // });
 
+     google.maps.event.addListenerOnce(map, 'idle', function() {
+         generateFramesData();
+     });
+
      myLayer.start(); //Starts the rendering loop.
+
  }
+
+ function generateFramesData() {
+
+     $.ajax({
+         url: "/data/frames_70000.json",
+         context: document.body,
+         beforeSend: function(xhr) {
+             toastr.info('Processing')
+         }
+     }).done(function(response) {
+ 
+         var res = {
+             'type': 'FeatureCollection',
+             'features': []
+         }
+         $.each(response, function(index, val) {
+             var lat = val.Geometry.Coordinates[0] 
+             var lng = val.Geometry.Coordinates[1] 
+             var point = {
+                 "geometry": {
+                     "type": "Point",
+                     "coordinates": [lng, lat]
+                 },
+                 "type": "Feature",
+                 "properties": val.Properties
+             }
+             res.features.push(point);
+         });
+         myLayer.loadData(res);
+         toastr.info('Done')
+     });
+
+ }
+
 
  function generateData(n) {
      var randInRange = function(from, to, fixed) {
@@ -59,7 +98,6 @@
          }
          res.features.push(point);
      }
-     console.log(res);
      myLayer.loadData(res);
  }
 
